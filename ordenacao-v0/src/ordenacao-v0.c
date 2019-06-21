ea8a0cb110b0eae7f7502e25ab6615db2268ff86
edneif <ednei_f@hotmail.com>
2019-06-21 09:02:24 -0300

    20190621@0900@

--- ordenacao-v0/src/ordenacao-v0.c
+++ ordenacao-v0/src/ordenacao-v0.c
@@ -16,7 +16,7 @@
 #include "ordenacao.h"
 
 #define tamanho_array 100000
-#define repeticoes 1
+#define repeticoes 20
 
 int main(void) {
 
@@ -26,96 +26,102 @@ int main(void) {
 	clock_t insertbinsort_start = 0, insertbinsort_end = 0,
 			insertbinsort_total = 0;
 	clock_t mergesort_start = 0, mergesort_end = 0, mergesort_total = 0;
-	clock_t selectionsort_start = 0, selectionsort_end = 0, selectionsort_total = 0;
+	clock_t selectionsort_start = 0, selectionsort_end = 0,
+			selectionsort_total = 0;
 
-	int i;
+	int i,j;
 
 	int *array = malloc(sizeof(int) * tamanho_array);
 	int *temp_array = malloc(sizeof(int) * tamanho_array);	//usada em merge
 
-	srand(time(NULL));
+	for (j = 1; j <= repeticoes; j++) {
 
-	for (i = 0; i < tamanho_array; i++) {
-		array[i] = rand() % tamanho_array;
+		printf("\n\ninteracao: %d de %d\n",j,repeticoes);
+		srand(time(NULL));
+
+		for (i = 0; i < tamanho_array; i++) {
+			array[i] = rand() % tamanho_array;
+		}
+		//puts("!!!iniciando Quicksort!!!\n");
+		quicksort_start = clock();
+		quicksort(array, 0, tamanho_array - 1); //usar posicao inicial e final vetor
+		quicksort_end = clock();
+		quicksort_total += quicksort_end - quicksort_start;
+		printf("\n| Quicksort T  | %f",
+				(double) quicksort_total / (j * CLOCKS_PER_SEC));
+		fflush(stdout);
+
+		srand(time(NULL));
+
+		for (i = 0; i < tamanho_array; i++) {
+			array[i] = rand() % tamanho_array;
+		}
+		//puts("!!!iniciando Insertsort!!!\n");
+		insertsort_start = clock();
+		insertsort(array, tamanho_array);
+		insertsort_end = clock();
+		insertsort_total += insertsort_end - insertsort_start;
+		printf("\n| InsertSort T | %f",
+				(double) insertsort_total / (j * CLOCKS_PER_SEC));
+		fflush(stdout);
+
+		srand(time(NULL));
+
+		for (i = 0; i < tamanho_array; i++) {
+			array[i] = rand() % tamanho_array;
+		}
+		//puts("!!!iniciando insertbinsort!!!\n");
+		insertbinsort_start = clock();
+		insertbinsort(array, tamanho_array);
+		insertbinsort_end = clock();
+		insertbinsort_total += insertbinsort_end - insertbinsort_start;
+		printf("\n| Insertbsort T| %f",
+				(double) insertbinsort_total / (j * CLOCKS_PER_SEC));
+		fflush(stdout);
+
+		srand(time(NULL));
+
+		for (i = 0; i < tamanho_array; i++) {
+			array[i] = rand() % tamanho_array;
+		}
+		//puts("!!!iniciando Bubble!!!\n");
+		buble_start = clock();
+		buble_sort(array, tamanho_array);
+		buble_end = clock();
+		buble_total += buble_end - buble_start;
+		printf("\n| Buble Total  | %f",
+				(double) buble_total / (j * CLOCKS_PER_SEC));
+
+		srand(time(NULL));
+
+		for (i = 0; i < tamanho_array; i++) {
+			array[i] = rand() % tamanho_array;
+		}
+		//puts("!!!iniciando MergeSort!!!\n");
+		mergesort_start = clock();
+		mergesort(array, 0, tamanho_array - 1, temp_array); //usar posicao inicial e final do vetor
+		mergesort_end = clock();
+		mergesort_total += mergesort_end - mergesort_start;
+		printf("\n| Merger Total  | %f",
+				(double) mergesort_total / (j * CLOCKS_PER_SEC));
+
+		srand(time(NULL));
+
+		for (i = 0; i < tamanho_array; i++) {
+			array[i] = rand() % tamanho_array;
+		}
+		//puts("!!!iniciando selectionsort!!!\n");
+		selectionsort_start = clock();
+		selection_sort(array, tamanho_array);
+		selectionsort_end = clock();
+		selectionsort_total += selectionsort_end - selectionsort_start;
+		printf("\n| Select Total  | %f",
+				(double) selectionsort_total / (j * CLOCKS_PER_SEC));
+
+		//int arraym[] = { 9, 8, 7, 3, 5, 4, 3, 2, 1, 10 };
+		//selection_sort(arraym, 10);
+
 	}
-	//puts("!!!iniciando Quicksort!!!\n");
-	quicksort_start = clock();
-//	quicksort(array, 0, tamanho_array - 1);    //usar posicao inicial e final vetor
-	quicksort_end = clock();
-	quicksort_total = quicksort_end - quicksort_start;
-	printf("\n| Quicksort T  | %f",
-			(double) quicksort_total / (repeticoes * CLOCKS_PER_SEC));
-	fflush(stdout);
-
-	srand(time(NULL));
-
-	for (i = 0; i < tamanho_array; i++) {
-		array[i] = rand() % tamanho_array;
-	}
-	//puts("!!!iniciando Insertsort!!!\n");
-	insertsort_start = clock();
-//	insertsort(array, tamanho_array);
-	insertsort_end = clock();
-	insertsort_total = insertsort_end - insertsort_start;
-	printf("\n| InsertSort T | %f",
-			(double) insertsort_total / (repeticoes * CLOCKS_PER_SEC));
-	fflush(stdout);
-
-	srand(time(NULL));
-
-	for (i = 0; i < tamanho_array; i++) {
-		array[i] = rand() % tamanho_array;
-	}
-	//puts("!!!iniciando insertbinsort!!!\n");
-	insertbinsort_start = clock();
-//	insertbinsort(array, tamanho_array);
-	insertbinsort_end = clock();
-	insertbinsort_total = insertbinsort_end - insertbinsort_start;
-	printf("\n| Insertbsort T| %f",
-			(double) insertbinsort_total / (repeticoes * CLOCKS_PER_SEC));
-	fflush(stdout);
-
-	srand(time(NULL));
-
-	for (i = 0; i < tamanho_array; i++) {
-		array[i] = rand() % tamanho_array;
-	}
-	//puts("!!!iniciando Bubble!!!\n");
-	buble_start = clock();
-//	buble_sort(array, tamanho_array);
-	buble_end = clock();
-	buble_total = buble_end - buble_start;
-	printf("\n| Buble Total  | %f",
-			(double) buble_total / (repeticoes * CLOCKS_PER_SEC));
-
-	srand(time(NULL));
-
-	for (i = 0; i < tamanho_array; i++) {
-		array[i] = rand() % tamanho_array;
-	}
-	//puts("!!!iniciando MergeSort!!!\n");
-	mergesort_start = clock();
-	mergesort(array, 0, tamanho_array - 1, temp_array); //usar posicao inicial e final do vetor
-	mergesort_end = clock();
-	mergesort_total = mergesort_end - mergesort_start;
-	printf("\n| Merger Total  | %f",
-			(double) mergesort_total / (repeticoes * CLOCKS_PER_SEC));
-
-	srand(time(NULL));
-
-	for (i = 0; i < tamanho_array; i++) {
-		array[i] = rand() % tamanho_array;
-	}
-	//puts("!!!iniciando selectionsort!!!\n");
-	selectionsort_start = clock();
-	selection_sort(array, tamanho_array);
-	selectionsort_end = clock();
-	selectionsort_total = selectionsort_end - selectionsort_start;
-	printf("\n| Select Total  | %f",
-			(double) selectionsort_total / (repeticoes * CLOCKS_PER_SEC));
-
-	//int arraym[] = { 9, 8, 7, 3, 5, 4, 3, 2, 1, 10 };
-	//selection_sort(arraym, 10);
 
 	puts("\n\n!!fim prog   !!!");
 	return EXIT_SUCCESS;
